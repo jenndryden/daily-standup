99e33eb0bbfab1adb61fdcc53f89507f59748b09
Jennifer Dryden <jenndryden8@gmail.com>
2021-04-10 17:32:14 -0400

    minor changes to title

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,7 +6,7 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Daily Stand-up</h1>
+        <h1 className="display-4">Daily Pizza</h1>
         <p className="lead">
           Keep your daily team agile Stand-up on time with adorable characters, smart timers, and a classic to-do list. Keep your team on track today!
         </p>
